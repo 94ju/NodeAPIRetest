487c1b8d7317ca9b10ee1c1674729b19cc786480
94ju <janithherath94@gmail.com>
2019-03-16 21:48:28 +0530

    Dirname

--- App.js
+++ App.js
@@ -4,12 +4,13 @@
 // setTimeout(function(){
 //     console.log(message);
 // },3000)
-var time =0 ;
+// var time =0 ;
 
-var timer=setInterval(function(){
-    time += 2;
-    console.log(time + " seconds has passed");
-    if(time>5){
-        clearInterval(timer)
-    }
-},2000)
+// var timer=setInterval(function(){
+//     time += 2;
+//     console.log(time + " seconds has passed");
+//     if(time>5){
+//         clearInterval(timer)
+//     }
+// },2000)
+console.log(__dirname);
